3ac9a33725d284365adfc293ba44accf9c43acc7
trimuerisandes <trimuerisandes@gmail.com>
2022-02-27 12:05:34 +0700

    merge to main

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
 <template>
   <v-app>
     <div
-      @mouseover="mouseNow=true"
-      @mouseleave="mouseNow=false"
+      @mouseover="mouseNow = true"
+      @mouseleave="mouseNow = false"
     >
       <v-navigation-drawer
         v-if="isAuth"
@@ -15,97 +15,60 @@
         app
         floating
         mini-variant-width="68"
-        :expand-on-hover="$vuetify.breakpoint.smAndUp?drawer:false"
-        :class="mouseNow?'nav-mouse-enter':''"
+        :mini-variant.sync="minivar"
+        :expand-on-hover="$vuetify.breakpoint.smAndUp ? drawer : false"
+        :class="mouseNow ? 'nav-mouse-enter' : ''"
       >
-        <v-list-item
-          class="pr-0 vertical-nav-menu-list"
-          style="height: 68px !important;"
-        >
-          <v-list-item-action>
-            <v-icon
-              color="white"
-              style="background: #4D77FF;border-radius:50px;padding:5px;margin-left:-5px"
-            >
-              mdi-cash-register
-            </v-icon>
-          </v-list-item-action>
+        <v-list-item>
           <v-list-item-content>
             <v-list-item-title
-              class="font-weight-bold text-uppercase text-left text-wrap"
-              style="color: #4D77FF;font-size:12px"
+              class="font-weight-bold text-uppercase text-center"
+              style="color: #007bff"
             >
-              e-Kasir Cofee & Auto Wash
+              Pest Control
             </v-list-item-title>
           </v-list-item-content>
           <v-list-item-action class="d-none d-md-block">
-            <v-icon
-              size="15"
-              @click="toggleDrawer"
-            >
-              {{ drawer?'mdi-arrow-expand-right':'mdi-arrow-expand-left' }}
+            <v-icon @click="toggleDrawer">
+              {{ drawer ? "mdi-radiobox-blank" : "mdi-record-circle-outline" }}
             </v-icon>
           </v-list-item-action>
         </v-list-item>
-
-        <v-divider />
-
-        <v-list
-          expand
-          shaped
-          class="pb-16"
-        >
-          <v-list-item-group
-            v-model="selectedItem"
-            color="primary"
-          >
-            <v-list-item
-              v-for="(item, index) in items"
-              :key="index"
-              :to="item.link"
-              class="vertical-nav-menu-list"
-              link
-            >
-              <v-list-item-icon>
-                <v-icon
-                  color="#8d8897"
-                  class="v-list-item-icon-custome"
-                >
-                  {{ item.icon }}
-                </v-icon>
-              </v-list-item-icon>
-
-              <v-list-item-content>
-                <v-list-item-title class="v-list-item-title-custome">
-                  {{ item.title }}
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
         <v-list-item
           class="pr-0 vertical-nav-menu-list"
-          style="height: 68px !important;padding-left: 13px !important;margin-top: 0 !important; padding-right: 10px !important;bottom: 0 !important;position: absolute;"
+          style="
+            height: 68px !important;
+            padding-left: 13px !important;
+            margin-top: 0 !important;
+            padding-right: 10px !important;
+          "
         >
           <v-list-item-icon class="mr-3">
             <v-avatar
-              color="#4D77FF"
-              size="35"
+              color="#2b92ff"
+              size="40"
             >
               <span class="white--text text-h5">
-                {{ user ? user.name.substring(0,1).toUpperCase() : '' }}
+                {{ user ? user.name.substring(0, 1).toUpperCase() : "" }}
               </span>
             </v-avatar>
           </v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title
               class="title"
-              style="font-size: 15px !important;"
+              style="font-size: 15px !important;white-space: nowrap !important;"
             >
-              {{ user ? user.name : '' }}
+              {{ user ? user.name : "" }}
             </v-list-item-title>
-            <v-list-item-subtitle style="word-wrap: normal !important;white-space: normal!important;font-size: 12px;text-transform: capitalize !important;">
-              {{ user ? user.role[0] : '' }}
+            <v-list-item-subtitle
+              style="
+                word-wrap: normal !important;
+                white-space: nowrap !important;
+                font-size: 14px;
+                text-transform: capitalize !important;
+              "
+            >
+              {{ user ? user.role[0] : "" }}
             </v-list-item-subtitle>
           </v-list-item-content>
           <v-list-item-action>
@@ -120,7 +83,7 @@
                   v-bind="attrs"
                   v-on="on"
                 >
-                  <v-icon>mdi-dots-horizontal</v-icon>
+                  <v-icon>mdi-dots-vertical</v-icon>
                 </v-btn>
               </template>
 
@@ -142,9 +105,21 @@
             </v-menu>
           </v-list-item-action>
         </v-list-item>
+
+        <v-divider />
+        <v-list
+          expand
+          shaped
+          class="pb-16"
+        >
+          <dynamic-menu
+            :menus="items"
+            :is-mini="minivar"
+          />
+        </v-list>
       </v-navigation-drawer>
     </div>
-    <v-main :class="'pt-1 '+($vuetify.breakpoint.smAndUp ? (drawer?'nav-mini':'') : '')">
+    <v-main :class="mainClass">
       <router-view @toggle-drawer="toggleDrawer" />
       <dialog-logout
         :show-dialog="showDialogLogout"
@@ -160,32 +135,40 @@
 <script>
 import { mapActions, mapGetters, mapState } from 'vuex'
 import Dialog from './components/Dialog'
-import menus from './router/menus'
+import  menus  from './router/menus'
 import { isEmpty } from '@/plugins/supports'
+import DynamicMenuVue from './components/DynamicMenu.vue'
 
 export default {
   name: 'App',
   components: {
-    'dialog-logout': Dialog
+    'dialog-logout': Dialog,
+    'dynamic-menu': DynamicMenuVue
   },
   data: () => ({
-    drawer: false, // set default expanded
+    drawer: true,
     openNav: true,
     mouseNow: true,
+    minivar: true,
     selectedItem: 1,
+    tes: { 'no-action': true, 'sub-group': true },
     showDialogLogout: false,
-    toolbarTitle: 'E-Office',
+    toolbarTitle: 'Pest Control',
     items: []
   }),
   computed: {
     ...mapGetters(['isAuth']),
-    ...mapState(['user'])
+    ...mapState(['user', 'perm']),
+    mainClass () {
+      return 'pt-1 ' +
+        (this.$vuetify.breakpoint.smAndUp ? (this.drawer ? 'nav-mini' : '') : '')
+    }
   },
   watch: {
     $route (to, from) {
       this.toolbarTitle = to.meta.title
       if (!isEmpty(this.user)) {
-        this.authRefresh().then(data => {
+        this.authRefresh().then((data) => {
           this.setupMenu(data || [])
           if (this.command === 'logout') {
             this.postLogout()
@@ -195,47 +178,59 @@ export default {
     }
   },
   created () {
-    this.setupMenu(this.user.perm)
+    this.setupMenu(this.perm)
     this.$vuetify.theme.dark = false
   },
   methods: {
     ...mapActions(['logout', 'authRefresh']),
+    test () {
+      return <v-list-item-title>Test</v-list-item-title>
+    },
     setupMenu (perm) {
       this.items = []
       for (let i = 0; i < menus.length; i++) {
-        const { path, meta } = menus[i]
-        const { requirePermission, icon, title, subheader } = meta
+        this.items.push(menus[i].subheader ? menus[i] : this.genMenus(menus[i], perm))
+      }
+    },
+    genMenus (menus, perm) {
+      console.log(JSON.stringify(menus))
+      const { path, meta } = menus
+      const { requirePermission, icon, title, subheader } = meta
 
-        if (!isEmpty(this.user)) {
-          if (requirePermission && icon && !isEmpty(perm)) {
-            let allow = false
-            if (perm.includes(requirePermission)) {
-              allow = true
-            }
+      if (!isEmpty(this.user)) {
+        if (requirePermission && icon && !isEmpty(perm)) {
+          let allow = false
+          if (perm.includes(requirePermission)) {
+            allow = true
+          }
 
-            if (allow) {
-              if (icon) {
-                const tmp = { title: title, icon: icon, link: path }
-                if (subheader) {
-                  let alreadyExist = false
-                  for (const item of this.items) {
-                    if (subheader === item.subheader) {
-                      alreadyExist = true
-                    }
-                  }
-                  if (!alreadyExist) {
-                    tmp.subheader = subheader
+          if (allow) {
+            if (icon) {
+              const tmp = { title: title, icon: icon }
+              tmp.path = path
+              if (subheader) {
+                let alreadyExist = false
+                for (const item of this.items) {
+                  if (subheader === item.subheader) {
+                    alreadyExist = true
                   }
                 }
-                this.items.push(tmp)
+                if (!alreadyExist) {
+                  tmp.subheader = subheader
+                }
               }
+              if (menus.children) {
+                tmp.children = menus.children.map((v) => this.genMenus(v, perm))
+                return tmp
+              }
+              return tmp
             }
           }
         }
       }
     },
     postLogout () {
-      this.logout().then(d => {
+      this.logout().then((d) => {
         this.showDialogLogout = false
         if (!this.isAuth) {
           this.$router.push({ name: 'login' })
@@ -258,91 +253,147 @@ export default {
 }
 </script>
 <style>
-  html {
-    overflow: scroll;
-    overflow-x: hidden;
-    -ms-overflow-style: none;  /* IE and Edge */
-    scrollbar-width: none;  /* Firefox */
-  }
+html {
+  overflow: scroll;
+  overflow-x: hidden;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
 
-  ::-webkit-scrollbar {
-    width: 0px; /* Remove scrollbar space */
-    background: transparent; /* Optional: just make scrollbar invisible */
-  }
+::-webkit-scrollbar {
+  width: 0px; /* Remove scrollbar space */
+  background: transparent; /* Optional: just make scrollbar invisible */
+}
 
-  /* Optional: show position indicator in red */
-  ::-webkit-scrollbar-thumb {
-    background: rgba(217, 217, 217, 0.99);
-  }
+/* Optional: show position indicator in red */
+::-webkit-scrollbar-thumb {
+  background: rgba(217, 217, 217, 0.99);
+}
 
-  .theme--light.v-application {
-    background: #f4f5fa !important;
-  }
-  .default--text {
-    color: rgba(94,86,105,.87) !important;
-  }
-  .nav-mini{
-    padding-left: 68px !important;
-  }
-  .header-mini{
-    padding-left: 0 !important;
-    left: 68px !important;
-  }
-  .theme--light.v-navigation-drawer {
-    background: #f4f5fa !important;
-  }
+.theme--light.v-application {
+  background: #f4f5fa !important;
+}
+.default--text {
+  color: rgba(94, 86, 105, 0.87) !important;
+}
+.nav-mini {
+  padding-left: 68px !important;
+}
+.header-mini {
+  padding-left: 0 !important;
+  left: 68px !important;
+}
+.theme--light.v-navigation-drawer {
+  background: #f4f5fa !important;
+}
 
-  .theme--light.v-app-bar.v-toolbar.v-sheet{
-    background: #f4f5fa !important;
-  }
-  .vertical-nav-menu-list.v-list-item--active:hover::before,
-  .vertical-nav-menu-list.v-list-item--active::before {
-    /*background: linear-gradient(98deg, #c48eff, #8b56eb 94%);*/
-    background: linear-gradient(98deg, #56c0ff, #2b92ff 94%);
-    opacity: 1 !important;
-  }
-  .vertical-nav-menu-list.theme--light.v-list-item {
-    color: rgba(94,86,105,.87) !important;
-  }
-  .vertical-nav-menu-list.theme--dark.v-list-item {
-    color: rgb(141, 136, 151) !important;
-  }
-  .vertical-nav-menu-list.v-list-item {
-    height: 44px !important;
-    min-height: 44px !important;
-    justify-content: flex-start !important;
-    padding: 0 18px 0 22px !important;
-  }
-  .vertical-nav-menu-list.v-list-item--active > .v-list-item__icon > .v-list-item-icon-custome {
-    color: #fff !important;
-  }
-  .vertical-nav-menu-list.v-list-item--active > .v-list-item__content > .v-list-item-title-custome {
-    color: #fff !important;
-    z-index: 1;
-  }
-  .vertical-nav-menu-list.theme--light.v-list-item--active {
-    box-shadow: 0 5px 10px -4px rgba(94,86,105,.42) !important;
-    transition: box-shadow .28s cubic-bezier(.4,0,.2,1) !important;
-  }
+.theme--light.v-app-bar.v-toolbar.v-sheet {
+  background: #f4f5fa !important;
+}
+.vertical-nav-menu-list.v-list-item--active:hover::before,
+.vertical-nav-menu-list.v-list-item--active::before {
+  /*background: linear-gradient(98deg, #c48eff, #8b56eb 94%);*/
+  background: linear-gradient(98deg, #56c0ff, #2b92ff 94%);
+  opacity: 1 !important;
+}
+.vertical-nav-menu-list > .v-list-group__header:hover::before {
+  background-color: #eeeff5 !important;
+}
+.vertical-nav-menu-list
+> .v-list-group__header.theme--light.v-list-item
+> .v-list-item__icon
+> .v-icon,
+.vertical-nav-menu-list
+> .v-list-group__header.theme--light.v-list-item
+> .v-list-item-title-custome,
+.vertical-nav-menu-list
+> .v-list-group__header.theme--light.v-list-item
+> .v-list-item__content
+> .v-list-item-title-custome {
+  color: #756e7f !important;
+}
+.vertical-nav-menu-list.theme--light.v-list-item {
+  color: rgba(94, 86, 105, 0.87) !important;
+}
+.vertical-nav-menu-list
+> .v-list-group__header.theme--dark.v-list-item
+> .v-list-item__icon
+> .v-icon,
+.vertical-nav-menu-list
+> .v-list-group__header.theme--dark.v-list-item
+> .v-list-item-title-custome,
+.vertical-nav-menu-list
+> .v-list-group__header.theme--dark.v-list-item
+> .v-list-item__content
+> .v-list-item-title-custome {
+  color: #756e7f !important;
+}
+.vertical-nav-menu-list.theme--dark.v-list-item {
+  color: rgb(141, 136, 151) !important;
+}
+.vertical-nav-menu-list > .v-list-group__header.v-list-item,
+.vertical-nav-menu-list.v-list-item {
+  height: 44px !important;
+  min-height: 44px !important;
+  justify-content: flex-start !important;
+  padding: 0 18px 0 22px !important;
+}
+.vertical-nav-menu-list.v-list-item--active
+> .v-list-item__icon
+> .v-list-item-icon-custome {
+  color: #fff !important;
+}
+.vertical-nav-menu-list.v-list-item--active
+> .v-list-item__content
+> .v-list-item-title-custome {
+  color: #fff !important;
+  z-index: 1;
+}
+.vertical-nav-menu-list.theme--light.v-list-item--active {
+  box-shadow: 0 5px 10px -4px rgba(94, 86, 105, 0.42) !important;
+  transition: box-shadow 0.28s cubic-bezier(0.4, 0, 0.2, 1) !important;
+}
 
-  .vertical-nav-menu-list.v-list-item > .v-list-item__icon {
-    margin-right: 12px !important;
-    align-self: center !important;
-  }
-  .vertical-nav-menu-list.v-list-item > .v-list-item__content > .v-list-item-title-custome {
-    line-height: 1.2 !important;
-    margin-top: 3px !important;
-  }
-  .vertical-nav-menu-list.v-list-item {
-    margin-top: .375rem !important;
-  }
-  .vertical-nav-menu-list.v-list-item:first-child {
-    margin-top: 0 !important;
-  }
-  .vertical-nav-menu-list.v-list-item {
-    padding: 0 18px 0 22px !important;
-  }
-  .nav-mouse-enter{
-    box-shadow:0 5px 6px -3px rgba(94,86,105,.2),0 3px 16px 2px rgba(94,86,105,.12),0 9px 12px 1px rgba(94,86,105,.14)
-  }
+.vertical-nav-menu-list
+> .v-list-group__header.v-list-item
+> .v-list-item__icon,
+.vertical-nav-menu-list.v-list-item > .v-list-item__icon {
+  margin-right: 12px !important;
+  align-self: center !important;
+}
+.vertical-nav-menu-list
+> .v-list-group__header.v-list-item
+> .v-list-item__content
+> .v-list-item-title-custome,
+.vertical-nav-menu-list.v-list-item
+> .v-list-item__content
+> .v-list-item-title-custome {
+  line-height: 1.2 !important;
+  margin-top: 3px !important;
+}
+.vertical-nav-menu-list > .v-list-group__header.v-list-item,
+.vertical-nav-menu-list.v-list-item {
+  margin-top: 0.375rem !important;
+}
+.vertical-nav-menu-list.v-list-item:first-child {
+  margin-top: 0 !important;
+}
+.vertical-nav-menu-list > .v-list-group__header.v-list-item,
+.vertical-nav-menu-list.v-list-item {
+  padding: 0 18px 0 22px !important;
+}
+.nav-mouse-enter {
+  box-shadow: 0 5px 6px -3px rgba(94, 86, 105, 0.2),
+  0 3px 16px 2px rgba(94, 86, 105, 0.12),
+  0 9px 12px 1px rgba(94, 86, 105, 0.14);
+}
+.v-list-item--active
+> .v-list-group__header__append-icon
+> .v-icon {
+  transform: rotate(-90deg) !important;
+}
+.v-list-group__header > .v-list-item__icon.v-list-group__header__append-icon {
+  margin-left: 4px !important;
+  min-width: auto !important;
+}
 </style>
--- src/router/menus/base/user.js
+++ src/router/menus/base/user.js
@@ -6,47 +6,63 @@ const title = 'User'
 const User = [
   {
     path: path,
-    name: routeName,
-    component: () => import(/* webpackChunkName: "[request].chunk" */ `@/views/${folder}/Index`),
+    component: () => import(/* webpackChunkName: "[request].chunk" */ `@/views/${folder}/Main`),
     meta: {
       title: title,
       icon: 'mdi-account-circle-outline',
       // subheader: '-',
       requiresAuth: true,
       requirePermission: permission + 'list'
-    }
-  },
-  {
-    path: path + '/baru',
-    name: routeName + '_add',
-    component: () => import(/* webpackChunkName: "[request].chunk" */ `@/views/${folder}/Add`),
-    meta: {
-      title: 'Tambah' + title,
-      requiresAuth: true,
-      requirePermission: permission + 'create'
-    }
-  },
-  {
-    path: path + '/view/:id',
-    name: routeName + '_view',
-    component: () => import(/* webpackChunkName: "[request].chunk" */ `@/views/${folder}/View`),
-    props: true,
-    meta: {
-      title: 'Detail' + title,
-      requiresAuth: true,
-      requirePermission: permission + 'list'
-    }
-  },
-  {
-    path: path + '/edit/:id',
-    name: routeName + '_edit',
-    component: () => import(/* webpackChunkName: "[request].chunk" */ `@/views/${folder}/Edit`),
-    props: true,
-    meta: {
-      title: 'Edit' + title,
-      requiresAuth: true,
-      requirePermission: permission + 'edit'
-    }
+    },
+    children: [
+      {
+        path: 'index',
+        name: routeName,
+        component: () => import(/* webpackChunkName: "[request].chunk" */ `@/views/${folder}/Index`),
+        meta: {
+          title: title,
+          icon: 'mdi-minus',
+          // subheader: '-',
+          requiresAuth: true,
+          requirePermission: permission + 'list'
+        }
+      },
+      {
+        path: 'baru',
+        name: routeName + '_add',
+        component: () => import(/* webpackChunkName: "[request].baru.chunk" */ `@/views/${folder}/Add`),
+        meta: {
+          title: 'Tambah ' + title,
+          icon: 'mdi-minus',
+          requiresAuth: true,
+          requirePermission: permission + 'create'
+        }
+      },
+      {
+        path: 'view/:id',
+        name: routeName + '_view',
+        component: () => import(/* webpackChunkName: "[request].view.chunk" */ `@/views/${folder}/View`),
+        props: true,
+        meta: {
+          title: 'Detail ' + title,
+          // icon: 'mdi-minus',
+          requiresAuth: true,
+          requirePermission: permission + 'list'
+        }
+      },
+      {
+        path: 'edit/:id',
+        name: routeName + '_edit',
+        component: () => import(/* webpackChunkName: "[request].edit.chunk" */ `@/views/${folder}/Edit`),
+        props: true,
+        meta: {
+          title: 'Edit ' + title,
+          // icon: 'mdi-minus',
+          requiresAuth: true,
+          requirePermission: permission + 'edit'
+        }
+      }
+    ]
   }
 ]
 
