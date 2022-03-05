8b080b0f0558c708800508c70f684c235306068c
trimuerisandes <trimuerisandes@gmail.com>
2022-03-05 12:31:35 +0700

    produk backend

--- api/database/seeds/ProdukSeeder.php
+++ api/database/seeds/ProdukSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\barang;
+use App\Models\produk;
 use Illuminate\Database\Seeder;
 
 class ProdukSeeder extends Seeder
@@ -14,5 +16,13 @@ class ProdukSeeder extends Seeder
     public function run()
     {
         //
+        //
+        $produk_collection = [
+            ['id_produk'=>'P01','nama_produk'=>'Hot Coffe Milk', 'id_kategori_produk'=>'KP01', 'harga'=>'20000','kesediaan'=>'tersedia','satuan'=>'SB01','deskripsi'=>'Kopi nikmat 120 cc','status'=>'aktif','stok'=>'120','type_bisnis'=>'coffeshop'],
+            ['id_produk'=>'P02','nama_produk'=>'Hot Coffe Latte', 'id_kategori_produk'=>'KP01', 'harga'=>'25000','kesediaan'=>'tersedia','satuan'=>'SB01','deskripsi'=>'Kopi nikmat 120 cc','status'=>'aktif','stok'=>'100','type_bisnis'=>'coffeshop']
+        ];
+        foreach($produk_collection as $item){
+            produk::create($item);
+        }
     }
 }
