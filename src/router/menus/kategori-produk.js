0cba3d131aab86149c2e52d0216904186adaeaa5
trimuerisandes <trimuerisandes@gmail.com>
2022-03-05 12:31:24 +0700

    produk backend

---
+++ src/router/menus/kategori-produk.js
@@ -0,0 +1,53 @@
+const path = '/kategori-produk'
+const permission = 'kategori-pegawai-'
+const routeName = 'kategori-produk'
+const folder = 'kategori-produk'
+const title = 'Kategori Produk'
+const KategoriProduk = [
+  {
+    path: path,
+    name: routeName,
+    component: () => import(/* webpackChunkName: "[request].chunk" */ `@/views/${folder}/Index`),
+    meta: {
+      title: title,
+      icon: 'mdi-key-outline',
+      // subheader: '-',
+      requiresAuth: true,
+      requirePermission: permission + 'list'
+    }
+  },
+  {
+    path: path + '/baru',
+    name: routeName + '_add',
+    component: () => import(/* webpackChunkName: "[request].chunk" */ `@/views/${folder}/Add`),
+    meta: {
+      title: 'Tambah' + title,
+      requiresAuth: true,
+      requirePermission: permission + 'create'
+    }
+  },
+  {
+    path: path + '/view/:id',
+    name: routeName + '_view',
+    component: () => import(/* webpackChunkName: "[request].chunk" */ `@/views/${folder}/View`),
+    props: true,
+    meta: {
+      title: 'Detail' + title,
+      requiresAuth: true,
+      requirePermission: permission + 'list'
+    }
+  },
+  {
+    path: path + '/edit/:id',
+    name: routeName + '_edit',
+    component: () => import(/* webpackChunkName: "[request].chunk" */ `@/views/${folder}/Edit`),
+    props: true,
+    meta: {
+      title: 'Edit' + title,
+      requiresAuth: true,
+      requirePermission: permission + 'edit'
+    }
+  }
+]
+
+export default KategoriProduk
